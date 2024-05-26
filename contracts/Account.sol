4d61c2de7344c59f7aa7c046d106e6305dabca34
Manuel David Romero Guerrero <manueldavidromeroguerrero@iMac.local>
2024-05-27 01:49:05 +0200

    first commit

---
+++ contracts/Account.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "@account-abstraction/contracts/core/EntryPoint.sol";
+import "@account-abstraction/contracts/interfaces/IAccount.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
+
+contract Account is IAccount {
+    uint public count;
+    address public owner;
+
+    constructor (address _owner) {
+        owner = _owner;
+    }
+
+    function validateUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256) external view returns (uint256 validationData) {
+        address recovered = ECDSA.recover(MessageHashUtils.toEthSignedMessageHash(userOpHash), userOp.signature);
+        return owner == recovered ? 0 : 1;
+    }
+
+    function execute() external {
+        count++;
+    }
+}
+
+contract AccountFactory {
+    function createAccount (address owner) external returns (address) {
+        Account acc = new Account(owner);
+        return address(acc);
+    }
+}
---
+++ scripts/test.js
@@ -0,0 +1,17 @@
+const hre = require("hardhat");
+
+const ACCOUNT_ADDR = "0xa16E02E87b7454126E5E10d957A927A7F5B5d2be";
+
+
+async function main() {
+    const account = await hre.ethers.getContractAt("Account", ACCOUNT_ADDR);
+    const count = await account.count();
+    
+    console.log(count);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+}
+)
